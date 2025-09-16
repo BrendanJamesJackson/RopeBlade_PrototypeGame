31a3b5c92fa036d6cdf5bac7d95c6015ba80f08d
BrendanJamesJackson <brendanjj50@gmail.com>
2025-09-16 16:46:20 +0200

    Developed environment shaders

---
+++ RopeBlade_Prototype/Assets/Scripts/RigidbodyDebugger.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class RigidbodyDebugger : MonoBehaviour
+{
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Debug.Log("Gravity: " + Physics.gravity);
+        Debug.Log("Simulation mode: " + Physics.simulationMode);
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log("Velocity: " + rb.linearVelocity + " Position: " + transform.position);
+    }
+}
